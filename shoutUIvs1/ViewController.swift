204d5d49bf3543ebbcd39f814087f11ac4b2942e
Jessica Ngo <jessican-go@hotmail.com>
2018-07-07 18:19:07 -0400

    Initial Commit

---
+++ shoutUIvs1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shoutUIvs1
+//
+//  Created by Jessica Ngo on 2018-07-07.
+//  Copyright © 2018 Jessica Ngo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ shoutUIvs1Tests/shoutUIvs1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  shoutUIvs1Tests.swift
+//  shoutUIvs1Tests
+//
+//  Created by Jessica Ngo on 2018-07-07.
+//  Copyright © 2018 Jessica Ngo. All rights reserved.
+//
+
+import XCTest
+@testable import shoutUIvs1
+
+class shoutUIvs1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
